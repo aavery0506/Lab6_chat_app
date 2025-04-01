f043acf4a5ae25cbc74117f01c17a6bf67529d61
aavery0506 <avery.alison.s@gmail.com>
2025-04-01 19:08:45 -0400

    first commit

---
+++ app/src/main/java/com/example/lab6_chat_app/Chat.java
@@ -0,0 +1,34 @@
+package com.example.lab6_chat_app;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.lab6_chat_app.databinding.ActivityChatBinding;
+
+public class Chat extends AppCompatActivity {
+
+    ActivityChatBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+
+        binding = ActivityChatBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
